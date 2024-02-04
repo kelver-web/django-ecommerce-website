be534baa057ebf37044253a05dc4c7f5fbf2d240
kelver-web <kelverwt@gmail.com>
2024-02-04 13:24:26 -0300

    add data of db in templates

--- store/views.py
+++ store/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from . models import Product
+
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'store/home.html')
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
+
