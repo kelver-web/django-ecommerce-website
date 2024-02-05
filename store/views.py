7315895bc0bc6af3cf0da471d0b8ed5db5f55ed7
kelver-web <kelverwt@gmail.com>
2024-02-05 19:57:55 -0300

    add page login and logout

--- store/views.py
+++ store/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 from . models import Product
 
@@ -12,3 +14,24 @@ def home(request):
 
 def about(request):
     return render(request,'store/about.html', {})
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,'Login successful')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return redirect('login')
+    return render(request,'store/login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.error(request, 'You have been logged out, Thanks for stopping by...')
+    return redirect('login')
